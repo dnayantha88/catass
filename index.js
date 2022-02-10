4cc1292bf5ebe4da9991658da3488da0a90381f7
DanushaNayantha <danusha.nayantha@1billiontech.com>
2022-02-10 10:04:16 +0530

    added main function

---
+++ index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import minimist from 'minimist';
+import mergeImg from 'merge-img';
+import { baseUrl } from './constants.js';
+
+// parse argument options
+let argv = minimist(process.argv.slice(2));
+let { greeting = 'Hello', who = 'You', width = 400, height = 500, color = 'Pink', size = 100 } = argv;
+
+// generate urls for requests
+const urlCreator = param => `${baseUrl+param}?width=${width}&height=${height}&color${color}&s=${size}`;
+
+// generating requests
+const requestCreator = message => axios.get(urlCreator(message), { responseType: 'arraybuffer' });
+
+// fetch images and merge
+async function createImage() {
+    try {
+        const [firstImg, secImg] = await axios.all([requestCreator(greeting), requestCreator(who)]);
+        mergeImg([Buffer.from(firstImg.data, 'binary'), Buffer.from(secImg.data, 'binary')])
+            .then(img => img.write('catass.png', () => console.log('Image saved!')));
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+createImage();
